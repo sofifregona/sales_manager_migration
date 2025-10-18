28f168b97d3a4d69aac5aa4f4f957ef63b9bbb15
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:35:58 +0200

    feat(backend/provider): add Provider entity

---
+++ backend/src/modules/provider/provider.entity.ts
@@ -0,0 +1,28 @@
+﻿import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+@Entity()
+export class Provider {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar" })
+  name!: string;
+
+  @Column({ name: "normalized_name", type: "varchar" })
+  normalizedName!: string;
+
+  @Column("bigint", { nullable: true })
+  cuit!: number | null;
+
+  @Column("bigint", { nullable: true })
+  telephone!: number | null;
+
+  @Column("varchar", { nullable: true })
+  email!: string | null;
+
+  @Column("varchar", { nullable: true })
+  address!: string | null;
+
+  @Column({ type: "boolean", default: true })
+  active!: boolean;
+}
+

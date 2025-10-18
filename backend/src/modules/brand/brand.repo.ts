08539a45f58f336a2ae9f25771eded603c56604e
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:34:55 +0200

    feat(backend/brand): add repository
    Encapsulate data access for brands with a narrow, testable API.

---
+++ backend/src/modules/brand/brand.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Brand } from "./brand.entity.js";
+
+export const brandRepo = AppDataSource.getRepository(Brand);

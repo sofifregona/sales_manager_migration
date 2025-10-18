3b57d4465e88bdec0e2466ea397d356e9b871bd2
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:53:14 +0200

    feat(routes/employee): minimal route wiring to Employee panel
    Delegate to loader/action and render EmployeePanelScreen.

---
+++ app/routes/employee.tsx
@@ -0,0 +1,12 @@
+﻿import { employeeLoader } from "~/feature/employee/employee-loader.server";
+export { employeeLoader as loader };
+import { employeeAction } from "~/feature/employee/employee-action.server";
+export { employeeAction as action };
+import {
+  EmployeePanelScreen,
+  EmployeePanelErrorBoundary,
+} from "~/feature/employee/screens/EmployeePanelScreen";
+
+export default EmployeePanelScreen;
+export { EmployeePanelErrorBoundary as ErrorBoundary };
+

c0aae10185f7462de5c7f740bee1d53d21a17b04
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:49:38 +0200

    types(transaction): add domain types
    Centralize transaction/movement types to reduce duplication and tighten safety.

---
+++ app/feature/transaction/transaction.d.ts
@@ -0,0 +1,46 @@
+﻿import type { AccountDTO } from "~/feature/account/account";
+import type { SaleDTO } from "~/feature/sale/types/sale";
+import type { Flash } from "~/types/flash";
+
+export interface TransactionDTO {
+  id: number;
+  dateTime: Date;
+  account: AccountDTO;
+  type: "income" | "expense";
+  origin: "sale" | "movement";
+  sale: SaleDTO | null;
+  amount: number;
+  description: string | null;
+}
+
+export interface CreateTransactionPayload {
+  idAccount: number;
+  amount: number;
+}
+
+export interface CreateMovementPayload
+  extends Partial<CreateTransactionPayload> {
+  type: "income" | "expense";
+  origin: "movement";
+  description?: string | null;
+}
+
+export interface CreateSaleMovementPayload
+  extends Partial<CreateTransactionPayload> {
+  origin: "sale";
+  saleId: number;
+}
+
+export interface UpdateMovementPayload extends Partial<CreateMovementPayload> {
+  id: number;
+  type: "income" | "expense";
+  amount: number;
+  description?: string | null;
+}
+
+export interface TransactionLoaderData {
+  accounts: AccountDTO[] | [];
+  transactions: TransactionDTO[] | [];
+  editingTransaction: TransactionDTO | null;
+  flash: Flash;
+}

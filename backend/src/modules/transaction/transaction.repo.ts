21f584d8910f26a5cd752053b9ff5a28f09f341d
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:36:48 +0200

    feat(backend/transaction): add repository
    Encapsulate data access for transactions with a narrow, testable API.

---
+++ backend/src/modules/transaction/transaction.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Transaction } from "./transaction.entity.js";
+
+export const transactionRepo = AppDataSource.getRepository(Transaction);

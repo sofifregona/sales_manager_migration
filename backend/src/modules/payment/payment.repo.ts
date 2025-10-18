bbfb54dc537a094df4c32f2b11e2632fc84e7dc6
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:35:37 +0200

    feat(backend/payment): add repository
    Encapsulate data access for payments with a narrow, testable API.

---
+++ backend/src/modules/payment/payment.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Payment } from "./payment.entity.js";
+
+export const paymentRepo = AppDataSource.getRepository(Payment);

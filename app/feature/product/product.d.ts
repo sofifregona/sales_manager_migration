14966ea1756a710005f33efeccb5311ae3f4e4bb
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:45:57 +0200

    types(product): add domain types
    Centralize product types to reduce duplication and tighten safety.

---
+++ app/feature/product/product.d.ts
@@ -0,0 +1,43 @@
+﻿import type { BrandDTO } from "~/feature/brand/types/brand";
+import type { CategoryDTO } from "~/feature/category/types/category";
+import type { ProviderDTO } from "~/feature/provider/types/provider";
+import type { Flash } from "~/types/flash";
+
+export interface ProductDTO {
+  id: number;
+  code: number;
+  name: string;
+  normalizedName: string;
+  price: number;
+  category: CategoryDTO | null;
+  provider: ProviderDTO | null;
+  brand: BrandDTO | null;
+  active: boolean;
+}
+
+export interface CreateProductPayload {
+  code: number;
+  name: string;
+  price: number;
+  idCategory?: number | null;
+  idProvider?: number | null;
+  idBrand?: number | null;
+}
+
+export interface UpdateProductPayload extends CreateProductPayload {
+  id: number;
+}
+
+export interface IncrementPricePayload {
+  ids: number[] | [];
+  percent: number;
+}
+
+export interface ProductLoaderData {
+  brands: BrandDTO[] | [];
+  categories: CategoryDTO[] | [];
+  providers: ProviderDTO[] | [];
+  products: ProductDTO[] | [];
+  editingProduct: ProductDTO | null;
+  flash: Flash;
+}

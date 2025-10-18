2c23bdaf57bcb4d83d5f268e699ce3e3b49c127e
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:35:59 +0200

    feat(backend/provider): add repository

---
+++ backend/src/modules/provider/provider.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Provider } from "./provider.entity.js";
+
+export const providerRepo = AppDataSource.getRepository(Provider);

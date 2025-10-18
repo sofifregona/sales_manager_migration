a77d8514ec8391d5c349c0c279f2e31f1b2d39a5
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:35:50 +0200

    feat(backend/product): add repository
    Encapsulate data access for products with a narrow, testable API.

---
+++ backend/src/modules/product/product.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Product } from "./product.entity.js";
+
+export const productRepo = AppDataSource.getRepository(Product);

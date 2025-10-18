27d031f0d71c959c16e3dcdf2f8745995ef15ca3
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:35:11 +0200

    feat(backend/category): register routes

---
+++ backend/src/modules/category/category.routes.ts
@@ -0,0 +1,37 @@
+﻿import { Router } from "express";
+import {
+  createCategoryHandler,
+  getAllCategoriesHandler,
+  getCategoryByIdHandler,
+  deactivateCategoryHandler,
+  reactivateCategoryHandler,
+  updateCategoryHandler,
+} from "./category.controller.js";
+import { requireRole } from "../../shared/guards/auth.guard.js";
+
+const router = Router();
+
+router.get("/categories", getAllCategoriesHandler);
+router.get("/categories/:id", getCategoryByIdHandler);
+router.post(
+  "/categories",
+  requireRole(["ADMIN", "MANAGER"]),
+  createCategoryHandler
+);
+router.patch(
+  "/categories/:id",
+  requireRole(["ADMIN", "MANAGER"]),
+  updateCategoryHandler
+);
+router.patch(
+  "/categories/:id/deactivate",
+  requireRole(["ADMIN"]),
+  deactivateCategoryHandler
+);
+router.patch(
+  "/brands/:id/reactivate",
+  requireRole(["ADMIN", "MANAGER"]),
+  reactivateCategoryHandler
+);
+
+export default router;

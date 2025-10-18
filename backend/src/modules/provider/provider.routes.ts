c2ebe7e82dba852277d5b1369c3f3820ca11af22
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:36:03 +0200

    feat(backend/provider): register routes

---
+++ backend/src/modules/provider/provider.routes.ts
@@ -0,0 +1,37 @@
+﻿import { Router } from "express";
+import {
+  createProviderHandler,
+  getAllProvidersHandler,
+  getProviderByIdHandler,
+  deactivateProviderHandler,
+  updateProviderHandler,
+  reactivateProviderHandler,
+} from "./provider.controller.js";
+import { requireRole } from "../../shared/guards/auth.guard.js";
+
+const router = Router();
+
+router.get("/providers", getAllProvidersHandler);
+router.get("/providers/:id", getProviderByIdHandler);
+router.post(
+  "/providers",
+  requireRole(["ADMIN", "MANAGER"]),
+  createProviderHandler
+);
+router.patch(
+  "/providers/:id",
+  requireRole(["ADMIN", "MANAGER"]),
+  updateProviderHandler
+);
+router.patch(
+  "/providers/:id/deactivate",
+  requireRole(["ADMIN"]),
+  deactivateProviderHandler
+);
+router.patch(
+  "/providers/:id/reactivate",
+  requireRole(["ADMIN", "MANAGER"]),
+  reactivateProviderHandler
+);
+
+export default router;

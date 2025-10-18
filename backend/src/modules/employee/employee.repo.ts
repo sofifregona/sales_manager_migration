6144fac6933920c210b88e780fdc0a477b2e11d4
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:35:21 +0200

    feat(backend/employee): add repository
    Encapsulate data access for employees with a narrow, testable API.

---
+++ backend/src/modules/employee/employee.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Employee } from "./employee.entity.js";
+
+export const employeeRepo = AppDataSource.getRepository(Employee);

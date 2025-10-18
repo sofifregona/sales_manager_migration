2bbbc55487da894709e77aa0f52d24534296f50b
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 13:34:36 +0200

    feat(backend/bartable): add repository
    Encapsulate data access for bartables with a narrow, testable API.

---
+++ backend/src/modules/bartable/bartable.repo.ts
@@ -0,0 +1,4 @@
+﻿import { AppDataSource } from "@back/src/shared/database/data-source.js";
+import { Bartable } from "./bartable.entity.js";
+
+export const bartableRepo = AppDataSource.getRepository(Bartable);

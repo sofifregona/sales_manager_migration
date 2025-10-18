786621538e1199c2eab9c9189fade64904601926
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:52:59 +0200

    feat(routes/category): minimal route wiring to Category panel
    Delegate to loader/action and render CategoryPanelScreen.

---
+++ app/routes/category.tsx
@@ -0,0 +1,12 @@
+﻿import { categoryLoader } from "~/feature/category/category-loader.server";
+export { categoryLoader as loader };
+import { categoryAction } from "~/feature/category/category-action.server";
+export { categoryAction as action };
+import {
+  CategoryPanelScreen,
+  CategoryPanelErrorBoundary,
+} from "~/feature/category/screens/CategoryPanelScreen";
+
+export default CategoryPanelScreen;
+export { CategoryPanelErrorBoundary as ErrorBoundary };
+

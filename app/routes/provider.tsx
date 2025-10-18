1e744e4be10e638747979d6f1b92c2ae4ba3754e
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:54:00 +0200

    feat(routes/provider): minimal route wiring to Provider panel
    Delegate to loader/action and render ProviderPanelScreen.

---
+++ app/routes/provider.tsx
@@ -0,0 +1,12 @@
+﻿import { providerLoader } from "~/feature/provider/provider-loader.server";
+export { providerLoader as loader };
+import { providerAction } from "~/feature/provider/provider-action.server";
+export { providerAction as action };
+import {
+  ProviderPanelScreen,
+  ProviderPanelErrorBoundary,
+} from "~/feature/provider/screens/ProviderPanelScreen";
+
+export default ProviderPanelScreen;
+export { ProviderPanelErrorBoundary as ErrorBoundary };
+

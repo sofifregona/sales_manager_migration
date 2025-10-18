154f15d00cdcec44f572db89e42e56d9ad15ccdc
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:53:40 +0200

    feat(routes/payment): minimal route wiring to Payment panel
    Delegate to loader/action and render PaymentPanelScreen.

---
+++ app/routes/payment.tsx
@@ -0,0 +1,12 @@
+﻿import { paymentLoader } from "~/feature/payment/payment-loader.server";
+export { paymentLoader as loader };
+import { paymentAction } from "~/feature/payment/payment-action.server";
+export { paymentAction as action };
+import {
+  PaymentPanelScreen,
+  PaymentPanelErrorBoundary,
+} from "~/feature/payment/screens/PaymentPanelScreen";
+
+export default PaymentPanelScreen;
+export { PaymentPanelErrorBoundary as ErrorBoundary };
+

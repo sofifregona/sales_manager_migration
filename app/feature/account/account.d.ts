61b0e9c7db1b45043395083f43d53219aedc6b3c
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:17:27 +0200

    types(account): add core types and schemas

---
+++ app/feature/account/account.d.ts
@@ -0,0 +1,24 @@
+﻿import type { Flash } from "~/types/flash";
+
+export interface AccountDTO {
+  id: number;
+  name: string;
+  normalizedName: string;
+  description: string | null;
+  active: boolean;
+}
+
+export interface CreateAccountPayload {
+  name: string;
+  description?: string | null;
+}
+
+export interface UpdateAccountPayload extends CreateAccountPayload {
+  id: number;
+}
+
+export interface AccountLoaderData {
+  accounts: AccountDTO[] | [];
+  editingAccount: AccountDTO | null;
+  flash: Flash;
+}

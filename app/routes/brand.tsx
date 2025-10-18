c6d98641efea57853d813b7029dd62acb7f8fd37
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:52:50 +0200

    feat(routes/brand): minimal route wiring to Brand panel
    Delegate to loader/action and render BrandPanelScreen.

---
+++ app/routes/brand.tsx
@@ -0,0 +1,12 @@
+﻿import { brandLoader } from "~/feature/brand/brand-loader.server";
+export { brandLoader as loader };
+import { brandAction } from "~/feature/brand/brand-action.server";
+export { brandAction as action };
+import {
+  BrandPanelScreen,
+  BrandPanelErrorBoundary,
+} from "~/feature/brand/screens/BrandPanelScreen";
+
+export default BrandPanelScreen;
+export { BrandPanelErrorBoundary as ErrorBoundary };
+

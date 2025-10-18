469cb9d98dcc08c2cd624eeca4c4dd8d92e611a1
Maria Sofia Fregona <sofi.fregona@gmail.com>
2025-10-18 12:52:40 +0200

    feat(routes/bartable): minimal route wiring to Bartable panel
    Delegate to loader/action and render BartablePanelScreen.

---
+++ app/routes/bartable.tsx
@@ -0,0 +1,12 @@
+﻿import { bartableLoader } from "~/feature/bartable/bartable-loader.server";
+export { bartableLoader as loader };
+import { bartableAction } from "~/feature/bartable/bartable-action.server";
+export { bartableAction as action };
+import {
+  BartablePanelScreen,
+  BartablePanelErrorBoundary,
+} from "~/feature/bartable/screens/BartablePanelScreen";
+
+export default BartablePanelScreen;
+export { BartablePanelErrorBoundary as ErrorBoundary };
+

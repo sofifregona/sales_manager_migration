ab7263d470f93b4d304c7104a870aab5c1525032
Maria Sofia Fregona <sofi.fregona@gmail.com>
2026-02-01 18:33:03 +0100

    feat: add payment method entity

---
+++ backend/src/modules/paymentMethod/payment-method.entity.ts
@@ -0,0 +1,26 @@
+﻿import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Account } from "../account/account.entity.js";
+
+@Entity()
+export class PaymentMethod {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", length: 80, nullable: false })
+  name!: string;
+
+  @Column({
+    name: "normalized_name",
+    type: "varchar",
+    length: 80,
+    unique: true,
+    nullable: false,
+  })
+  normalizedName!: string;
+
+  @ManyToOne(() => Account, { nullable: false })
+  account!: Account;
+
+  @Column({ type: "boolean", default: true, nullable: false })
+  active!: boolean;
+}

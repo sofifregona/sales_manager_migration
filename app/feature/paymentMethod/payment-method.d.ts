113d3760cca81081d015c493b27f718ec6036947
Maria Sofia Fregona <sofi.fregona@gmail.com>
2026-02-01 18:16:08 +0100

    feat: add payment method types

---
+++ app/feature/paymentMethod/payment-method.d.ts
@@ -0,0 +1,26 @@
+﻿import type { AccountDTO } from "~/feature/account/account";
+import type { Flash } from "~/types/flash";
+
+export interface PaymentMethodDTO {
+  id: number;
+  name: string;
+  normalizedName: string;
+  account: AccountDTO;
+  active: boolean;
+}
+
+export interface CreatePaymentMethodPayload {
+  name: string;
+  idAccount: number;
+}
+
+export interface UpdatePaymentMethodPayload extends CreatePaymentMethodPayload {
+  id: number;
+}
+
+export interface PaymentMethodLoaderData {
+  accounts: AccountDTO[] | [];
+  paymentMethods: PaymentMethodDTO[] | [];
+  editingPaymentMethod: PaymentMethodDTO | null;
+  flash: Flash;
+}

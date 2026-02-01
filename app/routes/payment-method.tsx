74955c5bb00e8ddaeec0976c599457ce176fb6fa
Maria Sofia Fregona <sofi.fregona@gmail.com>
2026-02-01 18:27:40 +0100

    feat: add payment method route

---
+++ app/routes/payment-method.tsx
@@ -0,0 +1,11 @@
+﻿import { paymentMethodLoader } from "~/feature/paymentMethod/payment-method-loader.server";
+export { paymentMethodLoader as loader };
+import { paymentMethodAction } from "~/feature/paymentMethod/payment-method-action.server";
+export { paymentMethodAction as action };
+import {
+  PaymentMethodPanelScreen,
+  PaymentMethodPanelErrorBoundary,
+} from "~/feature/paymentMethod/screens/PaymentMethodPanelScreen";
+
+export default PaymentMethodPanelScreen;
+export { PaymentMethodPanelErrorBoundary as ErrorBoundary };
